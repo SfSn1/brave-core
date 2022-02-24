7b55345734d99da5cbd79576bc6437a2528d405c
Mark Pilgrim <mpilgrim@brave.com>
2022-02-24 17:46:23 -0500

    add support for farbling HTTP header

--- browser/brave_content_browser_client.h
+++ browser/brave_content_browser_client.h
@@ -145,9 +145,6 @@ class BraveContentBrowserClient : public ChromeContentBrowserClient {
   blink::UserAgentMetadata GetUserAgentMetadata() override;
 
  private:
-  uint64_t session_token_;
-  uint64_t incognito_session_token_;
-
   void OnAllowGoogleAuthChanged();
 
   std::unique_ptr<PrefChangeRegistrar, content::BrowserThread::DeleteOnUIThread>
