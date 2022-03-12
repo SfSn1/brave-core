49ce693f1c9f30058ba7ac36a9ba3cec73d4dda2
sergey <spylogsster@gmail.com>
2022-03-12 17:06:25 +0300

    Convert Uint64 in JSON to string

--- components/brave_wallet/browser/fil_response_parser.cc
+++ components/brave_wallet/browser/fil_response_parser.cc
@@ -8,13 +8,9 @@
 #include <memory>
 #include <utility>
 
-#include "base/json/json_reader.h"
-#include "base/logging.h"
 #include "base/strings/string_number_conversions.h"
-#include "brave/components/brave_wallet/browser/brave_wallet_utils.h"
 #include "brave/components/brave_wallet/browser/json_rpc_response_parser.h"
-#include "brave/components/brave_wallet/common/brave_wallet_types.h"
-#include "brave/components/brave_wallet/common/hex_utils.h"
+#include "brave/components/json/rs/src/lib.rs.h"
 
 namespace brave_wallet {
 
@@ -22,15 +18,19 @@ bool ParseFilGetBalance(const std::string& json, std::string* balance) {
   return brave_wallet::ParseSingleStringResult(json, balance);
 }
 
-bool ParseFilGetTransactionCount(const std::string& json, uint64_t* count) {
-  base::Value result;
-  if (!ParseResult(json, &result))
+bool ParseFilGetTransactionCount(const std::string& raw_json, uint64_t* count) {
+  if (raw_json.empty())
     return false;
-  if (result.is_int() || result.is_double()) {
-    *count = result.GetDouble();
-    return true;
-  }
-  return false;
+  std::string sanitized_json(
+      json::convert_uint64_to_unicode_string("/result", raw_json.c_str()).c_str());
+  if (sanitized_json.empty())
+    return false;
+  std::string sanitized_value;
+  if (!brave_wallet::ParseSingleStringResult(sanitized_json,
+                                             &sanitized_value) ||
+      sanitized_value.empty())
+    return false;
+  return base::StringToUint64(sanitized_value, count);
 }
 
 }  // namespace brave_wallet
