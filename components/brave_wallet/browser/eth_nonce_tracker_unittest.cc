249005db13e2b00a4746942503c90342ae89e547
sergey <spylogsster@gmail.com>
2022-03-16 15:06:23 +0300

    Little improvements for {Fil|Eth}NonceTrackerUnittests

--- components/brave_wallet/browser/eth_nonce_tracker_unittest.cc
+++ components/brave_wallet/browser/eth_nonce_tracker_unittest.cc
@@ -6,10 +6,13 @@
 #include "brave/components/brave_wallet/browser/eth_nonce_tracker.h"
 
 #include <memory>
+#include <string>
 #include <utility>
 
 #include "base/test/bind.h"
+#include "base/test/task_environment.h"
 #include "brave/components/brave_wallet/browser/brave_wallet_constants.h"
+#include "brave/components/brave_wallet/browser/brave_wallet_prefs.h"
 #include "brave/components/brave_wallet/browser/brave_wallet_utils.h"
 #include "brave/components/brave_wallet/browser/eth_transaction.h"
 #include "brave/components/brave_wallet/browser/eth_tx_meta.h"
@@ -20,12 +23,7 @@
 #include "brave/components/brave_wallet/common/brave_wallet_types.h"
 #include "brave/components/brave_wallet/common/eth_address.h"
 #include "brave/components/brave_wallet/common/hex_utils.h"
-#include "chrome/browser/prefs/browser_prefs.h"
-#include "chrome/test/base/testing_browser_process.h"
-#include "chrome/test/base/testing_profile_manager.h"
-#include "components/prefs/pref_service.h"
 #include "components/sync_preferences/testing_pref_service_syncable.h"
-#include "content/public/test/browser_task_environment.h"
 #include "services/network/public/cpp/weak_wrapper_shared_url_loader_factory.h"
 #include "services/network/test/test_url_loader_factory.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -34,28 +32,40 @@ namespace brave_wallet {
 
 class EthNonceTrackerUnitTest : public testing::Test {
  public:
-  EthNonceTrackerUnitTest() {
+  EthNonceTrackerUnitTest()
+      : task_environment_(base::test::TaskEnvironment::TimeSource::MOCK_TIME) {
     shared_url_loader_factory_ =
         base::MakeRefCounted<network::WeakWrapperSharedURLLoaderFactory>(
             &url_loader_factory_);
   }
 
   void SetUp() override {
-    TestingProfile::Builder builder;
-    auto prefs =
-        std::make_unique<sync_preferences::TestingPrefServiceSyncable>();
-    RegisterUserProfilePrefs(prefs->registry());
-    builder.SetPrefService(std::move(prefs));
-    profile_ = builder.Build();
+    brave_wallet::RegisterProfilePrefs(prefs_.registry());
   }
 
-  PrefService* GetPrefs() { return profile_->GetPrefs(); }
+  PrefService* GetPrefs() { return &prefs_; }
 
   network::SharedURLLoaderFactory* shared_url_loader_factory() {
     return url_loader_factory_.GetSafeWeakWrapper().get();
   }
 
   void WaitForResponse() { task_environment_.RunUntilIdle(); }
+
+  bool GetNextNonce(EthNonceTracker* tracker,
+                    const std::string& address,
+                    uint256_t expected_nonce) {
+    bool callback_called = false;
+    bool callback_result = false;
+    tracker->GetNextNonce(
+        address, base::BindLambdaForTesting([&](bool success, uint256_t nonce) {
+          callback_called = true;
+          callback_result = success;
+          EXPECT_EQ(expected_nonce, nonce);
+        }));
+    WaitForResponse();
+    EXPECT_TRUE(callback_called);
+    return callback_result;
+  }
 
   void SetTransactionCount(uint256_t count) {
     transaction_count_ = count;
@@ -77,10 +87,10 @@ class EthNonceTrackerUnitTest : public testing::Test {
   }
 
   uint256_t transaction_count_ = 0;
+  base::test::TaskEnvironment task_environment_;
   network::TestURLLoaderFactory url_loader_factory_;
   scoped_refptr<network::SharedURLLoaderFactory> shared_url_loader_factory_;
-  content::BrowserTaskEnvironment task_environment_;
-  std::unique_ptr<TestingProfile> profile_;
+  sync_preferences::TestingPrefServiceSyncable prefs_;
 };
 
 TEST_F(EthNonceTrackerUnitTest, GetNonce) {
@@ -96,39 +106,19 @@ TEST_F(EthNonceTrackerUnitTest, GetNonce) {
 
   SetTransactionCount(2);
 
-  const std::string addr("0x2f015c60e0be116b1f0cd534704db9c92118fb6a");
-  uint256_t nonce_result = 0;
-  bool callback_called = false;
+  const std::string address("0x2f015c60e0be116b1f0cd534704db9c92118fb6a");
   // tx count: 2, confirmed: null, pending: null
-  nonce_tracker.GetNextNonce(
-      addr, base::BindLambdaForTesting([&](bool success, uint256_t nonce) {
-        callback_called = true;
-        if (success)
-          nonce_result = nonce;
-      }));
-  WaitForResponse();
-  EXPECT_TRUE(callback_called);
-  EXPECT_EQ(nonce_result, uint256_t(2));
+  EXPECT_TRUE(GetNextNonce(&nonce_tracker, address, 2));
 
   // tx count: 2, confirmed: [2], pending: null
   EthTxMeta meta;
   meta.set_id(TxMeta::GenerateMetaID());
-  meta.set_from(EthAddress::FromHex(addr).ToChecksumAddress());
+  meta.set_from(EthAddress::FromHex(address).ToChecksumAddress());
   meta.set_status(mojom::TransactionStatus::Confirmed);
   meta.tx()->set_nonce(uint256_t(2));
   tx_state_manager.AddOrUpdateTx(meta);
 
-  nonce_result = 0;
-  callback_called = false;
-  nonce_tracker.GetNextNonce(
-      addr, base::BindLambdaForTesting([&](bool success, uint256_t nonce) {
-        callback_called = true;
-        if (success)
-          nonce_result = nonce;
-      }));
-  WaitForResponse();
-  EXPECT_TRUE(callback_called);
-  EXPECT_EQ(nonce_result, uint256_t(3));
+  EXPECT_TRUE(GetNextNonce(&nonce_tracker, address, 3));
 
   // tx count: 2, confirmed: [2, 3], pending: null
   meta.set_id(TxMeta::GenerateMetaID());
@@ -136,17 +126,7 @@ TEST_F(EthNonceTrackerUnitTest, GetNonce) {
   meta.tx()->set_nonce(uint256_t(3));
   tx_state_manager.AddOrUpdateTx(meta);
 
-  nonce_result = 0;
-  callback_called = false;
-  nonce_tracker.GetNextNonce(
-      addr, base::BindLambdaForTesting([&](bool success, uint256_t nonce) {
-        callback_called = true;
-        if (success)
-          nonce_result = nonce;
-      }));
-  WaitForResponse();
-  EXPECT_TRUE(callback_called);
-  EXPECT_EQ(nonce_result, uint256_t(4));
+  EXPECT_TRUE(GetNextNonce(&nonce_tracker, address, 4));
 
   // tx count: 2, confirmed: [2, 3], pending: [4, 4]
   meta.set_status(mojom::TransactionStatus::Submitted);
@@ -156,17 +136,7 @@ TEST_F(EthNonceTrackerUnitTest, GetNonce) {
   meta.set_id(TxMeta::GenerateMetaID());
   tx_state_manager.AddOrUpdateTx(meta);
 
-  nonce_result = 0;
-  callback_called = false;
-  nonce_tracker.GetNextNonce(
-      addr, base::BindLambdaForTesting([&](bool success, uint256_t nonce) {
-        callback_called = true;
-        if (success)
-          nonce_result = nonce;
-      }));
-  WaitForResponse();
-  EXPECT_TRUE(callback_called);
-  EXPECT_EQ(nonce_result, uint256_t(5));
+  EXPECT_TRUE(GetNextNonce(&nonce_tracker, address, 5));
 }
 
 TEST_F(EthNonceTrackerUnitTest, NonceLock) {
@@ -184,31 +154,11 @@ TEST_F(EthNonceTrackerUnitTest, NonceLock) {
   base::Lock* lock = nonce_tracker.GetLock();
   lock->Acquire();
   const std::string addr("0x2f015c60e0be116b1f0cd534704db9c92118fb6a");
-  bool callback_called = false;
-  bool callback_success;
-  nonce_tracker.GetNextNonce(
-      addr, base::BindLambdaForTesting([&](bool success, uint256_t nonce) {
-        callback_called = true;
-        callback_success = success;
-      }));
-  WaitForResponse();
-  EXPECT_TRUE(callback_called);
-  EXPECT_FALSE(callback_success);
+  EXPECT_FALSE(GetNextNonce(&nonce_tracker, addr, 0));
 
   lock->Release();
 
-  uint256_t nonce_result = 0;
-  callback_called = false;
-  nonce_tracker.GetNextNonce(
-      addr, base::BindLambdaForTesting([&](bool success, uint256_t nonce) {
-        callback_called = true;
-        callback_success = success;
-        nonce_result = nonce;
-      }));
-  WaitForResponse();
-  EXPECT_TRUE(callback_called);
-  EXPECT_TRUE(callback_success);
-  EXPECT_EQ(nonce_result, uint256_t(4));
+  EXPECT_TRUE(GetNextNonce(&nonce_tracker, addr, 4));
 }
 
 }  // namespace brave_wallet
