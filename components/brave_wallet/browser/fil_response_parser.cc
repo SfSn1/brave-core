Astrogit-com/Astrosearch-core

Approve FIL transactions and added gas estimation

components/brave_wallet/browser/fil_response_parser.cc
@@ -4,7 +4,9 @@
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 #include "brave/components/brave_wallet/browser/fil_response_parser.h"
+#include <stdint.h>
 
+#include <charconv>
 #include <utility>
 
 #include "base/json/json_reader.h"
@@ -19,6 +21,51 @@ namespace brave_wallet {
 
 bool ParseFilGetBalance(const std::string& json, std::string* balance) {
   return brave_wallet::ParseSingleStringResult(json, balance);
+}
+
+bool ParseFilGetTransactionCount(const std::string& json, uint64_t* count) {
+  base::Value result;
+  if (!ParseResult(json, &result))
+    return false;
+  if (result.is_int() || result.is_double()) {
+    std::string value_string = base::NumberToString(result.GetDouble());
+    return base::StringToUint64(value_string, count);
+  }
+  return false;
+}
+
+bool ParseFilEstimateGas(const std::string& json,
+                         std::string* gas_premium,
+                         std::string* gas_fee_cap,
+                         uint64_t* gas_limit,
+                         std::string* cid) {
+  base::Value result;
+  if (!ParseResult(json, &result))
+    return false;
+  const base::DictionaryValue* result_dict = nullptr;
+  if (!result.GetAsDictionary(&result_dict))
+    return false;
+  auto limit = result_dict->FindDoubleKey("GasLimit");
+  if (!limit)
+    return false;
+  auto* premium = result_dict->FindStringKey("GasPremium");
+  if (!premium)
+    return false;
+  auto* fee_cap = result_dict->FindStringKey("GasFeeCap");
+  if (!fee_cap)
+    return false;
+  auto* cid_value = result_dict->FindKey("CID");
+  if (!cid_value)
+    return false;
+  auto* cid_root = cid_value->FindStringKey("/");
+  if (!cid_root)
+    return false;
+
+  *gas_fee_cap = *fee_cap;
+  *gas_limit = *limit;
+  *gas_premium = *premium;
+  *cid = *cid_root;
+  return true;
 }
 
 }  // namespace brave_wallet

SfSn1/brave-core

components/brave_wallet/browser/fil_response_parser.cc
@@ -33,4 +33,46 @@ bool ParseFilGetTransactionCount(const std::string& raw_json, uint64_t* count) {
   return base::StringToUint64(sanitized_value, count);
 }
 
+bool ParseFilEstimateGas(const std::string& raw_json,
+                         std::string* gas_premium,
+                         std::string* gas_fee_cap,
+                         int64_t* gas_limit,
+                         std::string* cid) {
+  if (raw_json.empty())
+    return false;
+  std::string sanitized_json(
+      json::convert_i64_to_string("/result/GasLimit", raw_json.c_str())
+          .c_str());
+  if (sanitized_json.empty())
+    return false;
+  base::Value result;
+  if (!ParseResult(sanitized_json, &result))
+    return false;
+  const base::DictionaryValue* result_dict = nullptr;
+  if (!result.GetAsDictionary(&result_dict))
+    return false;
+  auto* limit = result_dict->FindStringKey("GasLimit");
+  if (!limit)
+    return false;
+  auto* premium = result_dict->FindStringKey("GasPremium");
+  if (!premium)
+    return false;
+  auto* fee_cap = result_dict->FindStringKey("GasFeeCap");
+  if (!fee_cap)
+    return false;
+  auto* cid_value = result_dict->FindKey("CID");
+  if (!cid_value)
+    return false;
+  auto* cid_root = cid_value->FindStringKey("/");
+  if (!cid_root)
+    return false;
+
+  if (!base::StringToInt64(*limit, gas_limit))
+    return false;
+  *gas_fee_cap = *fee_cap;
+  *gas_premium = *premium;
+  *cid = *cid_root;
+  return true;
+}
+
 }  // namespace brave_wallet