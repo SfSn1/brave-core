Astrogit-com/Astrosearch-core

Share NonceTracker between Fil/Eth tx managers

browser/brave_wallet/fil_nonce_tracker_unittest.cc
@@ -1,25 +1,22 @@
-/* Copyright (c) 2021 The Brave Authors. All rights reserved.
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "brave/components/brave_wallet/browser/eth_nonce_tracker.h"
+#include "brave/components/brave_wallet/browser/fil_nonce_tracker.h"
 
 #include <memory>
 #include <utility>
 
+#include "base/strings/string_number_conversions.h"
 #include "base/test/bind.h"
 #include "brave/components/brave_wallet/browser/brave_wallet_constants.h"
 #include "brave/components/brave_wallet/browser/brave_wallet_utils.h"
-#include "brave/components/brave_wallet/browser/eth_transaction.h"
-#include "brave/components/brave_wallet/browser/eth_tx_meta.h"
-#include "brave/components/brave_wallet/browser/eth_tx_state_manager.h"
 #include "brave/components/brave_wallet/browser/json_rpc_service.h"
-#include "brave/components/brave_wallet/browser/tx_meta.h"
+#include "brave/components/brave_wallet/browser/fil_tx_meta.h"
+#include "brave/components/brave_wallet/browser/fil_tx_state_manager.h"
 #include "brave/components/brave_wallet/common/brave_wallet.mojom.h"
 #include "brave/components/brave_wallet/common/brave_wallet_types.h"
-#include "brave/components/brave_wallet/common/eth_address.h"
-#include "brave/components/brave_wallet/common/hex_utils.h"
 #include "chrome/browser/prefs/browser_prefs.h"
 #include "chrome/test/base/testing_browser_process.h"
 #include "chrome/test/base/testing_profile_manager.h"
@@ -32,9 +29,9 @@
 
 namespace brave_wallet {
 
-class EthNonceTrackerUnitTest : public testing::Test {
+class FilNonceTrackerUnitTest : public testing::Test {
  public:
-  EthNonceTrackerUnitTest() {
+  FilNonceTrackerUnitTest() {
     shared_url_loader_factory_ =
         base::MakeRefCounted<network::WeakWrapperSharedURLLoaderFactory>(
             &url_loader_factory_);
@@ -57,7 +54,7 @@ class EthNonceTrackerUnitTest : public testing::Test {
 
   void WaitForResponse() { task_environment_.RunUntilIdle(); }
 
-  void SetTransactionCount(uint256_t count) {
+  void SetTransactionCount(uint64_t count) {
     transaction_count_ = count;
     url_loader_factory_.ClearResponses();
 
@@ -65,39 +62,39 @@ class EthNonceTrackerUnitTest : public testing::Test {
     // http://localhost:7545 URL used below is coming from.
     url_loader_factory_.AddResponse(
         brave_wallet::GetNetworkURL(GetPrefs(), mojom::kLocalhostChainId,
-                                    mojom::CoinType::ETH)
+                                    mojom::CoinType::FIL)
             .spec(),
         GetResultString());
   }
 
  private:
   std::string GetResultString() const {
-    return "{\"id\":1,\"jsonrpc\":\"2.0\",\"result\":\"" +
-           Uint256ValueToHex(transaction_count_) + "\"}";
+    return "{\"id\":1,\"jsonrpc\":\"2.0\",\"result\":" +
+           base::NumberToString(transaction_count_) + "}";
   }
 
-  uint256_t transaction_count_ = 0;
+  uint64_t transaction_count_ = 0;
   network::TestURLLoaderFactory url_loader_factory_;
   scoped_refptr<network::SharedURLLoaderFactory> shared_url_loader_factory_;
   content::BrowserTaskEnvironment task_environment_;
   std::unique_ptr<TestingProfile> profile_;
 };
 
-TEST_F(EthNonceTrackerUnitTest, GetNonce) {
+TEST_F(FilNonceTrackerUnitTest, GetNonce) {
   JsonRpcService service(shared_url_loader_factory(), GetPrefs());
   base::RunLoop run_loop;
   service.SetNetwork(
-      brave_wallet::mojom::kLocalhostChainId, mojom::CoinType::ETH,
+      brave_wallet::mojom::kLocalhostChainId, mojom::CoinType::FIL,
       base::BindLambdaForTesting([&](bool success) { run_loop.Quit(); }));
   run_loop.Run();
 
-  EthTxStateManager tx_state_manager(GetPrefs(), &service);
-  EthNonceTracker nonce_tracker(&tx_state_manager, &service);
+  FilTxStateManager tx_state_manager(GetPrefs(), &service);
+  FilNonceTracker nonce_tracker(&tx_state_manager, &service);
 
   SetTransactionCount(2);
 
-  const std::string addr("0x2f015c60e0be116b1f0cd534704db9c92118fb6a");
-  uint256_t nonce_result = 0;
+  const std::string addr("t1lqarsh4nkg545ilaoqdsbtj4uofplt6sto26ziy");
+  uint64_t nonce_result = 0;
   bool callback_called = false;
   // tx count: 2, confirmed: null, pending: null
   nonce_tracker.GetNextNonce(
@@ -108,14 +105,14 @@ TEST_F(EthNonceTrackerUnitTest, GetNonce) {
       }));
   WaitForResponse();
   EXPECT_TRUE(callback_called);
-  EXPECT_EQ(nonce_result, uint256_t(2));
+  EXPECT_EQ(nonce_result, uint64_t(2));
 
   // tx count: 2, confirmed: [2], pending: null
-  EthTxMeta meta;
+  FilTxMeta meta;
   meta.set_id(TxMeta::GenerateMetaID());
-  meta.set_from(EthAddress::FromHex(addr).ToChecksumAddress());
+  meta.set_from(FilAddress::FromAddress(addr).ToChecksumAddress());
   meta.set_status(mojom::TransactionStatus::Confirmed);
-  meta.tx()->set_nonce(uint256_t(2));
+  meta.tx()->set_nonce(uint64_t(2));
   tx_state_manager.AddOrUpdateTx(meta);
 
   nonce_result = 0;
@@ -128,12 +125,12 @@ TEST_F(EthNonceTrackerUnitTest, GetNonce) {
       }));
   WaitForResponse();
   EXPECT_TRUE(callback_called);
-  EXPECT_EQ(nonce_result, uint256_t(3));
+  EXPECT_EQ(nonce_result, uint64_t(3));
 
   // tx count: 2, confirmed: [2, 3], pending: null
   meta.set_id(TxMeta::GenerateMetaID());
   meta.set_status(mojom::TransactionStatus::Confirmed);
-  meta.tx()->set_nonce(uint256_t(3));
+  meta.tx()->set_nonce(uint64_t(3));
   tx_state_manager.AddOrUpdateTx(meta);
 
   nonce_result = 0;
@@ -146,11 +143,11 @@ TEST_F(EthNonceTrackerUnitTest, GetNonce) {
       }));
   WaitForResponse();
   EXPECT_TRUE(callback_called);
-  EXPECT_EQ(nonce_result, uint256_t(4));
+  EXPECT_EQ(nonce_result, uint64_t(4));
 
   // tx count: 2, confirmed: [2, 3], pending: [4, 4]
   meta.set_status(mojom::TransactionStatus::Submitted);
-  meta.tx()->set_nonce(uint256_t(4));
+  meta.tx()->set_nonce(uint64_t(4));
   meta.set_id(TxMeta::GenerateMetaID());
   tx_state_manager.AddOrUpdateTx(meta);
   meta.set_id(TxMeta::GenerateMetaID());
@@ -166,24 +163,24 @@ TEST_F(EthNonceTrackerUnitTest, GetNonce) {
       }));
   WaitForResponse();
   EXPECT_TRUE(callback_called);
-  EXPECT_EQ(nonce_result, uint256_t(5));
+  EXPECT_EQ(nonce_result, uint64_t(5));
 }
 
-TEST_F(EthNonceTrackerUnitTest, NonceLock) {
+TEST_F(FilNonceTrackerUnitTest, NonceLock) {
   JsonRpcService service(shared_url_loader_factory(), GetPrefs());
   base::RunLoop run_loop;
   service.SetNetwork(
-      brave_wallet::mojom::kLocalhostChainId, mojom::CoinType::ETH,
+      brave_wallet::mojom::kLocalhostChainId, mojom::CoinType::FIL,
       base::BindLambdaForTesting([&](bool success) { run_loop.Quit(); }));
   run_loop.Run();
-  EthTxStateManager tx_state_manager(GetPrefs(), &service);
-  EthNonceTracker nonce_tracker(&tx_state_manager, &service);
+  FilTxStateManager tx_state_manager(GetPrefs(), &service);
+  FilNonceTracker nonce_tracker(&tx_state_manager, &service);
 
   SetTransactionCount(4);
 
   base::Lock* lock = nonce_tracker.GetLock();
   lock->Acquire();
-  const std::string addr("0x2f015c60e0be116b1f0cd534704db9c92118fb6a");
+  const std::string addr("t1lqarsh4nkg545ilaoqdsbtj4uofplt6sto26ziy");
   bool callback_called = false;
   bool callback_success;
   nonce_tracker.GetNextNonce(
@@ -197,7 +194,7 @@ TEST_F(EthNonceTrackerUnitTest, NonceLock) {
 
   lock->Release();
 
-  uint256_t nonce_result = 0;
+  uint64_t nonce_result = 0;
   callback_called = false;
   nonce_tracker.GetNextNonce(
       addr, base::BindLambdaForTesting([&](bool success, uint256_t nonce) {
@@ -208,7 +205,7 @@ TEST_F(EthNonceTrackerUnitTest, NonceLock) {
   WaitForResponse();
   EXPECT_TRUE(callback_called);
   EXPECT_TRUE(callback_success);
-  EXPECT_EQ(nonce_result, uint256_t(4));
+  EXPECT_EQ(nonce_result, uint64_t(4));
 }
 
 }  // namespace brave_wallet

components/brave_wallet/browser/eth_nonce_tracker.cc
@@ -3,39 +3,47 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+#include <string>
+
 #include "brave/components/brave_wallet/browser/eth_nonce_tracker.h"
-
-#include <algorithm>
-#include <memory>
-#include <utility>
-#include <vector>
-
-#include "base/bind.h"
-#include "brave/components/brave_wallet/browser/eth_transaction.h"
 #include "brave/components/brave_wallet/browser/eth_tx_meta.h"
-#include "brave/components/brave_wallet/browser/eth_tx_state_manager.h"
 #include "brave/components/brave_wallet/browser/json_rpc_service.h"
-#include "brave/components/brave_wallet/browser/tx_meta.h"
-#include "brave/components/brave_wallet/common/brave_wallet.mojom.h"
+#include "brave/components/brave_wallet/browser/nonce_tracker.h"
+#include "brave/components/brave_wallet/common/brave_wallet.mojom-shared.h"
 #include "brave/components/brave_wallet/common/eth_address.h"
 
 namespace brave_wallet {
 
-namespace {
+EthNonceTracker::EthNonceTracker(TxStateManager* tx_state_manager,
+                                 JsonRpcService* json_rpc_service)
+    : NonceTracker(tx_state_manager, json_rpc_service), weak_factory_(this) {}
+EthNonceTracker::~EthNonceTracker() = default;
 
-uint256_t GetHighestLocallyConfirmed(
+void EthNonceTracker::GetNextNonce(const std::string& from,
+                                   GetNextNonceCallback callback) {
+  GetJsonRpcService()->GetEthTransactionCount(
+      from,
+      base::BindOnce(&NonceTracker::OnGetNetworkNonce,
+                     weak_factory_.GetWeakPtr(), from, std::move(callback)));
+}
+
+std::string EthNonceTracker::GetChecksumAddress(const std::string& address) {
+  return EthAddress::FromHex(address).ToChecksumAddress();
+}
+
+uint256_t EthNonceTracker::GetHighestLocallyConfirmed(
     const std::vector<std::unique_ptr<TxMeta>>& metas) {
   uint256_t highest = 0;
   for (auto& meta : metas) {
-    auto* eth_meta = static_cast<EthTxMeta*>(meta.get());
+    auto* tx_meta = static_cast<EthTxMeta*>(meta.get());
     DCHECK(
-        eth_meta->tx()->nonce());  // Not supposed to happen for a confirmed tx.
-    highest = std::max(highest, eth_meta->tx()->nonce().value() + (uint256_t)1);
+        tx_meta->tx()->nonce());  // Not supposed to happen for a confirmed tx.
+    highest = std::max(highest, tx_meta->tx()->nonce().value() + (uint256_t)1);
   }
   return highest;
 }
 
-uint256_t GetHighestContinuousFrom(
+uint256_t EthNonceTracker::GetHighestContinuousFrom(
     const std::vector<std::unique_ptr<TxMeta>>& metas,
     uint256_t start) {
   uint256_t highest = start;
@@ -47,51 +55,6 @@ uint256_t GetHighestContinuousFrom(
       highest++;
   }
   return highest;
-}
-
-}  // namespace
-
-EthNonceTracker::EthNonceTracker(EthTxStateManager* tx_state_manager,
-                                 JsonRpcService* json_rpc_service)
-    : tx_state_manager_(tx_state_manager),
-      json_rpc_service_(json_rpc_service),
-      weak_factory_(this) {}
-EthNonceTracker::~EthNonceTracker() = default;
-
-void EthNonceTracker::GetNextNonce(const EthAddress& from,
-                                   GetNextNonceCallback callback) {
-  const std::string hex_address = from.ToHex();
-  json_rpc_service_->GetTransactionCount(
-      hex_address, base::BindOnce(&EthNonceTracker::OnGetNetworkNonce,
-                                  weak_factory_.GetWeakPtr(), EthAddress(from),
-                                  std::move(callback)));
-}
-
-void EthNonceTracker::OnGetNetworkNonce(EthAddress from,
-                                        GetNextNonceCallback callback,
-                                        uint256_t network_nonce,
-                                        mojom::ProviderError error,
-                                        const std::string& error_message) {
-  if (!nonce_lock_.Try()) {
-    std::move(callback).Run(false, network_nonce);
-    return;
-  }
-  auto confirmed_transactions = tx_state_manager_->GetTransactionsByStatus(
-      mojom::TransactionStatus::Confirmed, from);
-  uint256_t local_highest = GetHighestLocallyConfirmed(confirmed_transactions);
-
-  uint256_t highest_confirmed = std::max(network_nonce, local_highest);
-
-  auto pending_transactions = tx_state_manager_->GetTransactionsByStatus(
-      mojom::TransactionStatus::Submitted, from);
-
-  uint256_t highest_continuous_from =
-      GetHighestContinuousFrom(pending_transactions, highest_confirmed);
-
-  uint256_t nonce = std::max(network_nonce, highest_continuous_from);
-
-  nonce_lock_.Release();
-  std::move(callback).Run(true, nonce);
 }
 
 }  // namespace brave_wallet

SfSn1/brave-core

browser/brave_wallet/fil_nonce_tracker_unittest.cc
@@ -1,29 +1,23 @@
-/* Copyright (c) 2021 The Brave Authors. All rights reserved.
+/* Copyright (c) 2022 The Brave Authors. All rights reserved.
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "brave/components/brave_wallet/browser/eth_nonce_tracker.h"
+#include "brave/components/brave_wallet/browser/fil_nonce_tracker.h"
 
 #include <memory>
+#include <string>
 #include <utility>
 
+#include "base/strings/string_number_conversions.h"
 #include "base/test/bind.h"
-#include "brave/components/brave_wallet/browser/brave_wallet_constants.h"
 #include "brave/components/brave_wallet/browser/brave_wallet_utils.h"
-#include "brave/components/brave_wallet/browser/eth_transaction.h"
-#include "brave/components/brave_wallet/browser/eth_tx_meta.h"
-#include "brave/components/brave_wallet/browser/eth_tx_state_manager.h"
+#include "brave/components/brave_wallet/browser/fil_tx_meta.h"
+#include "brave/components/brave_wallet/browser/fil_tx_state_manager.h"
 #include "brave/components/brave_wallet/browser/json_rpc_service.h"
-#include "brave/components/brave_wallet/browser/tx_meta.h"
 #include "brave/components/brave_wallet/common/brave_wallet.mojom.h"
-#include "brave/components/brave_wallet/common/brave_wallet_types.h"
-#include "brave/components/brave_wallet/common/eth_address.h"
-#include "brave/components/brave_wallet/common/hex_utils.h"
 #include "chrome/browser/prefs/browser_prefs.h"
-#include "chrome/test/base/testing_browser_process.h"
 #include "chrome/test/base/testing_profile_manager.h"
-#include "components/prefs/pref_service.h"
 #include "components/sync_preferences/testing_pref_service_syncable.h"
 #include "content/public/test/browser_task_environment.h"
 #include "services/network/public/cpp/weak_wrapper_shared_url_loader_factory.h"
@@ -32,9 +26,9 @@
 
 namespace brave_wallet {
 
-class EthNonceTrackerUnitTest : public testing::Test {
+class FilNonceTrackerUnitTest : public testing::Test {
  public:
-  EthNonceTrackerUnitTest() {
+  FilNonceTrackerUnitTest() {
     shared_url_loader_factory_ =
         base::MakeRefCounted<network::WeakWrapperSharedURLLoaderFactory>(
             &url_loader_factory_);
@@ -57,7 +51,7 @@ class EthNonceTrackerUnitTest : public testing::Test {
 
   void WaitForResponse() { task_environment_.RunUntilIdle(); }
 
-  void SetTransactionCount(uint256_t count) {
+  void SetTransactionCount(uint64_t count) {
     transaction_count_ = count;
     url_loader_factory_.ClearResponses();
 
@@ -65,39 +59,39 @@ class EthNonceTrackerUnitTest : public testing::Test {
     // http://localhost:7545 URL used below is coming from.
     url_loader_factory_.AddResponse(
         brave_wallet::GetNetworkURL(GetPrefs(), mojom::kLocalhostChainId,
-                                    mojom::CoinType::ETH)
+                                    mojom::CoinType::FIL)
             .spec(),
         GetResultString());
   }
 
  private:
   std::string GetResultString() const {
-    return "{\"id\":1,\"jsonrpc\":\"2.0\",\"result\":\"" +
-           Uint256ValueToHex(transaction_count_) + "\"}";
+    return "{\"id\":1,\"jsonrpc\":\"2.0\",\"result\":" +
+           base::NumberToString(transaction_count_) + "}";
   }
 
-  uint256_t transaction_count_ = 0;
+  uint64_t transaction_count_ = 0;
   network::TestURLLoaderFactory url_loader_factory_;
   scoped_refptr<network::SharedURLLoaderFactory> shared_url_loader_factory_;
   content::BrowserTaskEnvironment task_environment_;
   std::unique_ptr<TestingProfile> profile_;
 };
 
-TEST_F(EthNonceTrackerUnitTest, GetNonce) {
+TEST_F(FilNonceTrackerUnitTest, GetNonce) {
   JsonRpcService service(shared_url_loader_factory(), GetPrefs());
   base::RunLoop run_loop;
   service.SetNetwork(
-      brave_wallet::mojom::kLocalhostChainId, mojom::CoinType::ETH,
+      brave_wallet::mojom::kLocalhostChainId, mojom::CoinType::FIL,
       base::BindLambdaForTesting([&](bool success) { run_loop.Quit(); }));
   run_loop.Run();
 
-  EthTxStateManager tx_state_manager(GetPrefs(), &service);
-  EthNonceTracker nonce_tracker(&tx_state_manager, &service);
+  FilTxStateManager tx_state_manager(GetPrefs(), &service);
+  FilNonceTracker nonce_tracker(&tx_state_manager, &service);
 
   SetTransactionCount(2);
 
-  const std::string addr("0x2f015c60e0be116b1f0cd534704db9c92118fb6a");
-  uint256_t nonce_result = 0;
+  const std::string addr("t1lqarsh4nkg545ilaoqdsbtj4uofplt6sto26ziy");
+  uint64_t nonce_result = 0;
   bool callback_called = false;
   // tx count: 2, confirmed: null, pending: null
   nonce_tracker.GetNextNonce(
@@ -108,14 +102,14 @@ TEST_F(EthNonceTrackerUnitTest, GetNonce) {
       }));
   WaitForResponse();
   EXPECT_TRUE(callback_called);
-  EXPECT_EQ(nonce_result, uint256_t(2));
+  EXPECT_EQ(nonce_result, uint64_t(2));
 
   // tx count: 2, confirmed: [2], pending: null
-  EthTxMeta meta;
+  FilTxMeta meta;
   meta.set_id(TxMeta::GenerateMetaID());
-  meta.set_from(EthAddress::FromHex(addr).ToChecksumAddress());
+  meta.set_from(FilAddress::FromAddress(addr).EncodeAsString());
   meta.set_status(mojom::TransactionStatus::Confirmed);
-  meta.tx()->set_nonce(uint256_t(2));
+  meta.tx()->set_nonce(uint64_t(2));
   tx_state_manager.AddOrUpdateTx(meta);
 
   nonce_result = 0;
@@ -128,12 +122,12 @@ TEST_F(EthNonceTrackerUnitTest, GetNonce) {
       }));
   WaitForResponse();
   EXPECT_TRUE(callback_called);
-  EXPECT_EQ(nonce_result, uint256_t(3));
+  EXPECT_EQ(nonce_result, uint64_t(3));
 
   // tx count: 2, confirmed: [2, 3], pending: null
   meta.set_id(TxMeta::GenerateMetaID());
   meta.set_status(mojom::TransactionStatus::Confirmed);
-  meta.tx()->set_nonce(uint256_t(3));
+  meta.tx()->set_nonce(uint64_t(3));
   tx_state_manager.AddOrUpdateTx(meta);
 
   nonce_result = 0;
@@ -146,11 +140,11 @@ TEST_F(EthNonceTrackerUnitTest, GetNonce) {
       }));
   WaitForResponse();
   EXPECT_TRUE(callback_called);
-  EXPECT_EQ(nonce_result, uint256_t(4));
+  EXPECT_EQ(nonce_result, uint64_t(4));
 
   // tx count: 2, confirmed: [2, 3], pending: [4, 4]
   meta.set_status(mojom::TransactionStatus::Submitted);
-  meta.tx()->set_nonce(uint256_t(4));
+  meta.tx()->set_nonce(uint64_t(4));
   meta.set_id(TxMeta::GenerateMetaID());
   tx_state_manager.AddOrUpdateTx(meta);
   meta.set_id(TxMeta::GenerateMetaID());
@@ -166,24 +160,24 @@ TEST_F(EthNonceTrackerUnitTest, GetNonce) {
       }));
   WaitForResponse();
   EXPECT_TRUE(callback_called);
-  EXPECT_EQ(nonce_result, uint256_t(5));
+  EXPECT_EQ(nonce_result, uint64_t(5));
 }
 
-TEST_F(EthNonceTrackerUnitTest, NonceLock) {
+TEST_F(FilNonceTrackerUnitTest, NonceLock) {
   JsonRpcService service(shared_url_loader_factory(), GetPrefs());
   base::RunLoop run_loop;
   service.SetNetwork(
-      brave_wallet::mojom::kLocalhostChainId, mojom::CoinType::ETH,
+      brave_wallet::mojom::kLocalhostChainId, mojom::CoinType::FIL,
       base::BindLambdaForTesting([&](bool success) { run_loop.Quit(); }));
   run_loop.Run();
-  EthTxStateManager tx_state_manager(GetPrefs(), &service);
-  EthNonceTracker nonce_tracker(&tx_state_manager, &service);
+  FilTxStateManager tx_state_manager(GetPrefs(), &service);
+  FilNonceTracker nonce_tracker(&tx_state_manager, &service);
 
   SetTransactionCount(4);
 
   base::Lock* lock = nonce_tracker.GetLock();
   lock->Acquire();
-  const std::string addr("0x2f015c60e0be116b1f0cd534704db9c92118fb6a");
+  const std::string addr("t1lqarsh4nkg545ilaoqdsbtj4uofplt6sto26ziy");
   bool callback_called = false;
   bool callback_success;
   nonce_tracker.GetNextNonce(
@@ -197,7 +191,7 @@ TEST_F(EthNonceTrackerUnitTest, NonceLock) {
 
   lock->Release();
 
-  uint256_t nonce_result = 0;
+  uint64_t nonce_result = 0;
   callback_called = false;
   nonce_tracker.GetNextNonce(
       addr, base::BindLambdaForTesting([&](bool success, uint256_t nonce) {
@@ -208,7 +202,7 @@ TEST_F(EthNonceTrackerUnitTest, NonceLock) {
   WaitForResponse();
   EXPECT_TRUE(callback_called);
   EXPECT_TRUE(callback_success);
-  EXPECT_EQ(nonce_result, uint256_t(4));
+  EXPECT_EQ(nonce_result, uint64_t(4));
 }
 
 }  // namespace brave_wallet

components/brave_wallet/browser/eth_nonce_tracker.cc
@@ -7,23 +7,35 @@
 
 #include <algorithm>
 #include <memory>
+#include <string>
 #include <utility>
 #include <vector>
 
-#include "base/bind.h"
-#include "brave/components/brave_wallet/browser/eth_transaction.h"
 #include "brave/components/brave_wallet/browser/eth_tx_meta.h"
-#include "brave/components/brave_wallet/browser/eth_tx_state_manager.h"
 #include "brave/components/brave_wallet/browser/json_rpc_service.h"
-#include "brave/components/brave_wallet/browser/tx_meta.h"
+#include "brave/components/brave_wallet/browser/nonce_tracker.h"
+#include "brave/components/brave_wallet/browser/tx_state_manager.h"
 #include "brave/components/brave_wallet/common/brave_wallet.mojom.h"
+#include "brave/components/brave_wallet/common/brave_wallet_types.h"
 #include "brave/components/brave_wallet/common/eth_address.h"
 
 namespace brave_wallet {
 
-namespace {
+EthNonceTracker::EthNonceTracker(TxStateManager* tx_state_manager,
+                                 JsonRpcService* json_rpc_service)
+    : NonceTracker(tx_state_manager, json_rpc_service), weak_factory_(this) {}
 
-uint256_t GetHighestLocallyConfirmed(
+EthNonceTracker::~EthNonceTracker() = default;
+
+void EthNonceTracker::GetNextNonce(const std::string& from,
+                                   GetNextNonceCallback callback) {
+  GetJsonRpcService()->GetEthTransactionCount(
+      from,
+      base::BindOnce(&EthNonceTracker::OnEthGetNetworkNonce,
+                     weak_factory_.GetWeakPtr(), from, std::move(callback)));
+}
+
+uint256_t EthNonceTracker::GetHighestLocallyConfirmed(
     const std::vector<std::unique_ptr<TxMeta>>& metas) {
   uint256_t highest = 0;
   for (auto& meta : metas) {
@@ -35,7 +47,7 @@ uint256_t GetHighestLocallyConfirmed(
   return highest;
 }
 
-uint256_t GetHighestContinuousFrom(
+uint256_t EthNonceTracker::GetHighestContinuousFrom(
     const std::vector<std::unique_ptr<TxMeta>>& metas,
     uint256_t start) {
   uint256_t highest = start;
@@ -49,49 +61,18 @@ uint256_t GetHighestContinuousFrom(
   return highest;
 }
 
-}  // namespace
-
-EthNonceTracker::EthNonceTracker(EthTxStateManager* tx_state_manager,
-                                 JsonRpcService* json_rpc_service)
-    : tx_state_manager_(tx_state_manager),
-      json_rpc_service_(json_rpc_service),
-      weak_factory_(this) {}
-EthNonceTracker::~EthNonceTracker() = default;
-
-void EthNonceTracker::GetNextNonce(const EthAddress& from,
-                                   GetNextNonceCallback callback) {
-  const std::string hex_address = from.ToHex();
-  json_rpc_service_->GetTransactionCount(
-      hex_address, base::BindOnce(&EthNonceTracker::OnGetNetworkNonce,
-                                  weak_factory_.GetWeakPtr(), EthAddress(from),
-                                  std::move(callback)));
-}
-
-void EthNonceTracker::OnGetNetworkNonce(EthAddress from,
-                                        GetNextNonceCallback callback,
-                                        uint256_t network_nonce,
-                                        mojom::ProviderError error,
-                                        const std::string& error_message) {
-  if (!nonce_lock_.Try()) {
-    std::move(callback).Run(false, network_nonce);
+void EthNonceTracker::OnEthGetNetworkNonce(const std::string& from,
+                                           GetNextNonceCallback callback,
+                                           uint256_t result,
+                                           mojom::ProviderError error,
+                                           const std::string& error_message) {
+  if (error != mojom::ProviderError::kSuccess) {
+    std::move(callback).Run(false, result);
     return;
   }
-  auto confirmed_transactions = tx_state_manager_->GetTransactionsByStatus(
-      mojom::TransactionStatus::Confirmed, from);
-  uint256_t local_highest = GetHighestLocallyConfirmed(confirmed_transactions);
-
-  uint256_t highest_confirmed = std::max(network_nonce, local_highest);
-
-  auto pending_transactions = tx_state_manager_->GetTransactionsByStatus(
-      mojom::TransactionStatus::Submitted, from);
-
-  uint256_t highest_continuous_from =
-      GetHighestContinuousFrom(pending_transactions, highest_confirmed);
-
-  uint256_t nonce = std::max(network_nonce, highest_continuous_from);
-
-  nonce_lock_.Release();
-  std::move(callback).Run(true, nonce);
+  auto nonce =
+      GetFinalNonce(EthAddress::FromHex(from).ToChecksumAddress(), result);
+  std::move(callback).Run(nonce.has_value(), nonce.has_value() ? *nonce : 0);
 }
 
 }  // namespace brave_wallet