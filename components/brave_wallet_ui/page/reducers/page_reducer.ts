Astrogit-com/Astrosearch-core

fix(wallet): reset swap input fields after swap

components/brave_wallet_ui/page/reducers/page_reducer.ts
@@ -170,7 +170,7 @@ reducer.on(Actions.setMetaMaskInitialized, (state: PageState, payload: boolean) 
   }
 })
 
-reducer.on(Actions.setPageSwapQuote, (state: any, payload: BraveWallet.SwapResponse) => {
+reducer.on(Actions.setPageSwapQuote, (state: any, payload: BraveWallet.SwapResponse | undefined) => {
   return {
     ...state,
     swapQuote: payload

SfSn1/brave-core

components/brave_wallet_ui/page/reducers/page_reducer.ts
@@ -170,7 +170,7 @@ reducer.on(Actions.setMetaMaskInitialized, (state: PageState, payload: boolean) 
   }
 })
 
-reducer.on(Actions.setPageSwapQuote, (state: any, payload: BraveWallet.SwapResponse) => {
+reducer.on(Actions.setPageSwapQuote, (state: PageState, payload: BraveWallet.SwapResponse | undefined) => {
   return {
     ...state,
     swapQuote: payload